16cf909bd19506c66025fc6532422c3b18d72685
08abhinav <abhinavnegi101@gmail.com>
2025-07-27 22:39:09 +0530

    feat: created UserRoutes function

--- backend/routes/user_routes.go
+++ backend/routes/user_routes.go
@@ -3,11 +3,18 @@ package routes
 import (
 	"github.com/08abhinav/cropLink/controllers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/08abhinav/cropLink/shared"
 )
 
-func UserRoutes(app *fiber.App){
-	api := app.Group("/api")
+func UserRoutes(app *fiber.App, repo *shared.Repos) {
+	api := app.Group("/api/user")
 	app.Get("/", controllers.HomePage)
-	api.Post("/register", controllers.RegisterUser)
-	api.Post("/signin", controllers.SigninUser)
+
+	api.Post("/register", func(c *fiber.Ctx) error {
+		return controllers.RegisterUser(c, repo.DB)
+	})
+
+	api.Post("/signin", func(c *fiber.Ctx) error {
+		return controllers.SigninUser(c, repo.DB)
+	})
 }
