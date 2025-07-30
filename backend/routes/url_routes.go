0221771d3603314be9da9bb90d85372ac1ea77d4
08abhinav <abhinavnegi101@gmail.com>
2025-07-30 23:01:44 +0530

    update: added url routes for creation for authenticate users only

--- backend/routes/url_routes.go
+++ backend/routes/url_routes.go
@@ -2,12 +2,23 @@ package routes
 
 import (
 	"github.com/08abhinav/cropLink/controllers"
+	"github.com/08abhinav/cropLink/middleware"
 	"github.com/gofiber/fiber/v2"
+	"github.com/08abhinav/cropLink/shared"
 )
 
-func UrlRoutes(app *fiber.App)error{
-	api := app.Group("/api")
+func UrlRoutes(app *fiber.App, repo *shared.Repos) {
+	api := app.Group("/api/url", middleware.JWTMiddleware())
 
-	api.Post("/crateUrl", controllers.CreateShortUrl)
-	return nil
+	api.Post("/createUrl", func(c *fiber.Ctx) error {
+		return controllers.CreateShortUrl(c, repo.DB)
+	})
+
+	api.Get("/my-urls", func(c *fiber.Ctx) error {
+		return controllers.GetUserUrls(c, repo.DB)
+	})
+
+	api.Get("/u/:short", func(c *fiber.Ctx) error {
+		return controllers.RedirectUrl(c, repo.DB)
+	})
 }
