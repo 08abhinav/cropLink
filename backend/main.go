2101c77c610dd56ab84e89d734c41428b6b13dba
08abhinav <abhinavnegi101@gmail.com>
2025-07-30 23:07:32 +0530

    main.go

--- backend/main.go
+++ backend/main.go
@@ -1,23 +1,31 @@
-package backend
+package main
 
 import (
 	"log"
 	"os"
 
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/08abhinav/cropLink/model"
 	"github.com/08abhinav/cropLink/routes"
+	"github.com/08abhinav/cropLink/shared"
 	"github.com/08abhinav/cropLink/storage"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
 
+
 func main() {
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowCredentials: true,
+	}))
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("can't load .evn", err)
+		log.Fatal("can't load .env file:", err)
 	}
 
+	os.Getenv("JWT_SECRETKEY")
 	config := &storage.Config{
 		Host:     os.Getenv("DB_HOST"),
 		Port:     os.Getenv("DB_PORT"),
@@ -29,16 +37,20 @@ func main() {
 
 	db, err := storage.NewConnection(config)
 	if err != nil {
-		log.Fatal("config error", err)
+		log.Fatal("database connection failed:", err)
 	}
 
 	err = model.MigrateModels(db)
 	if err != nil {
-		log.Fatal("migration error", err)
+		log.Fatal("migration error:", err)
 	}
 
-	routes.UserRoutes(app)
-	routes.UrlRoutes(app) 
+	repo := &shared.Repos{
+		DB: db,
+	}
 
-	app.Listen(":8080")
+	routes.UserRoutes(app, repo)
+	routes.UrlRoutes(app, repo)
+
+	log.Fatal(app.Listen(":8080"))
 }
