a0c7c5c64b3c4ca0ec1f8135d2d931eedbb8a6ec
08abhinav <abhinavnegi101@gmail.com>
2025-08-04 18:32:23 +0530

    update: resolve the redirection issue

--- frontend/src/lib/ProtectedRoute.tsx
+++ frontend/src/lib/ProtectedRoute.tsx
@@ -1,14 +1,23 @@
-import { useAuth } from "@/context/AuthContext"
-import React from "react"
 import { Navigate } from "react-router-dom";
+import { useAuth } from "@/context/AuthContext";
+import React, { type JSX } from "react";
 
-const ProtectRoute = ({children}: {children: React.ReactNode}) =>{
-    const {isAuthenticated} = useAuth();
+const ProtectRoute = ({ children }: { children: JSX.Element }) => {
+  const { isAuthenticated, authChecked } = useAuth();
 
-    if(!isAuthenticated){
-        return <Navigate to="/signin" replace />
-    }
-    return <>{children}</>
-}
+  if (!authChecked) {
+    return (
+      <div className="flex items-center justify-center h-full p-8">
+        <div className="text-gray-500">Checking authentication...</div>
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/signin" replace />;
+  }
+
+  return children;
+};
 
 export default ProtectRoute;
