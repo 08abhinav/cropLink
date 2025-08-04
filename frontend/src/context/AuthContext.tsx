c98d0da2a9d484da3aa3c69601de2d8046280193
08abhinav <abhinavnegi101@gmail.com>
2025-08-04 18:30:33 +0530

    update: resolve the baseURL issue, checkAuth and logout function working

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -12,6 +12,7 @@ interface AuthContextType {
   setIsAuthenticated: (val: boolean) => void;
   logout: () => Promise<void>;
   checkAuth: () => Promise<void>;
+  authChecked: boolean;
 }
 
 const AuthContext = createContext<AuthContextType>({
@@ -19,23 +20,28 @@ const AuthContext = createContext<AuthContextType>({
   setIsAuthenticated: () => {},
   logout: async () => {},
   checkAuth: async () => {},
+  authChecked: false,
 });
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [authChecked, setAuthChecked] = useState(false);
 
   const checkAuth = useCallback(async () => {
     try {
-      await axios.get("/user/me", { withCredentials: true });
+      await axios.get("api/user/me", { withCredentials: true });
       setIsAuthenticated(true);
     } catch {
+      console.error("Auth check failed:");
       setIsAuthenticated(false);
+    } finally {
+      setAuthChecked(true);
     }
   }, []);
 
   const logout = useCallback(async () => {
     try {
-      await axios.post("/user/logout", {}, { withCredentials: true });
+      await axios.get("api/user/logout", { withCredentials: true });
       setIsAuthenticated(false);
     } catch (err) {
       console.error("Logout error:", err);
@@ -48,7 +54,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <AuthContext.Provider
-      value={{ isAuthenticated, setIsAuthenticated, logout, checkAuth }}
+      value={{ isAuthenticated, setIsAuthenticated, logout, checkAuth, authChecked }}
     >
       {children}
     </AuthContext.Provider>
