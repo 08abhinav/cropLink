87428c5ef1443bc3bd74e2d691ddc909158c6bdd
08abhinav <abhinavnegi101@gmail.com>
2025-08-21 23:00:48 +0530

    update: added clerk's component for checking the auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,46 @@
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Signin from './pages/Signin';
-import DashBoard from './pages/DashBoard';
-import ProtectRoute from './lib/ProtectedRoute';
+
+import Home from "./pages/Home";
+import DashBoard from "./pages/DashBoard";
+import UrlCreation from "./pages/UrlCreation";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import ProtectedRoute from "./lib/ProtectedRoute";
 
 const App = () => {
   return (
     <>
+      <header>
+        <SignedOut>
+          <SignInButton forceRedirectUrl="/dashboard"/>
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectRoute>
-              <DashBoard />
-            </ProtectRoute>
-          }
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/dashboard" 
+        element={
+          <ProtectedRoute>
+            <DashBoard /> 
+          </ProtectedRoute>}
         />
+        <Route path="/urlCreation" 
+        element={
+          <ProtectedRoute>
+            <UrlCreation />
+          </ProtectedRoute>
+        } />
       </Routes>
 
       <ToastContainer position="top-right" autoClose={1500} />
