820d6980f424d5e42cabc367b0015b7b2a903ed6
08abhinav <abhinavnegi101@gmail.com>
2025-08-21 23:00:04 +0530

    update: Added Clerk provider for authentication

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { AuthProvider } from './context/AuthContext.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <App/>
-      </AuthProvider>
+      </ClerkProvider>
     </BrowserRouter>
   </StrictMode>
 )
