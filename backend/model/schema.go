e061e623f55c7d1f15b46dfd75ae9e36b2207a84
08abhinav <abhinavnegi101@gmail.com>
2025-09-22 15:46:34 +0530

    update: added a field for customurl also

--- backend/model/schema.go
+++ backend/model/schema.go
@@ -8,10 +8,11 @@ import (
 type Url struct {
 	ID          uint      `gorm:"primaryKey; autoIncrement" json:"id"`
 	OriginalUrl string    `json:"original_url"`
-	ShortUrl    string    `json:"short_url"`
+	ShortUrl    string    `json:"short_url" gorm:"uniqueIndex"`
+	IsCustom    bool      `json:"is_custom" gorm:"default:false"`
 	Clicked     uint      `json:"clicked"`
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UserID string `json:"user_id"`
+	UserID      string    `json:"user_id"`
 }
 
 func MigrateModels(db *gorm.DB) error {
