4b1d7400e2c6b44bdbecc64ee74a6db707855826
08abhinav <abhinavnegi101@gmail.com>
2025-09-22 16:08:13 +0530

    feat: added a new entry for Expire url

--- backend/model/schema.go
+++ backend/model/schema.go
@@ -6,13 +6,14 @@ import (
 )
 
 type Url struct {
-	ID          uint      `gorm:"primaryKey; autoIncrement" json:"id"`
-	OriginalUrl string    `json:"original_url"`
-	ShortUrl    string    `json:"short_url" gorm:"uniqueIndex"`
-	IsCustom    bool      `json:"is_custom" gorm:"default:false"`
-	Clicked     uint      `json:"clicked"`
-	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UserID      string    `json:"user_id"`
+	ID          uint       `gorm:"primaryKey; autoIncrement" json:"id"`
+	OriginalUrl string     `json:"original_url"`
+	ShortUrl    string     `json:"short_url" gorm:"uniqueIndex"`
+	IsCustom    bool       `json:"is_custom" gorm:"default:false"`
+	Clicked     uint       `json:"clicked"`
+	ExpiresAt   *time.Time `json:"expires_at"`
+	CreatedAt   time.Time  `json:"created_at" gorm:"autoCreateTime"`
+	UserID      string     `json:"user_id"`
 }
 
 func MigrateModels(db *gorm.DB) error {
