229b2df0f8b88a56018dc2ee36bc5f826334220e
08abhinav <abhinavnegi101@gmail.com>
2025-09-23 16:03:18 +0530

    update: created context for checking authorization

--- frontend/src/lib/ProtectedRoute.tsx
+++ frontend/src/lib/ProtectedRoute.tsx
@@ -1,23 +1,21 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "@/context/AuthContext";
-import React, { type JSX } from "react";
+import { useUser, RedirectToSignIn } from "@clerk/clerk-react";
 
-const ProtectRoute = ({ children }: { children: JSX.Element }) => {
-  const { isAuthenticated, authChecked } = useAuth();
-
-  if (!authChecked) {
-    return (
-      <div className="flex items-center justify-center h-full p-8">
-        <div className="text-gray-500">Checking authentication...</div>
-      </div>
-    );
-  }
-
-  if (!isAuthenticated) {
-    return <Navigate to="/signin" replace />;
-  }
-
-  return children;
+type Props = {
+  children: React.ReactNode;
 };
 
-export default ProtectRoute;
+const ProtectedRoute = ({ children }: Props) => {
+  const { isLoaded, isSignedIn } = useUser();
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isSignedIn) {
+    return <RedirectToSignIn />;
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
