f88b3ab31887f147bb2e27cc9d1271a5c882fbb5
08abhinav <abhinavnegi101@gmail.com>
2026-03-16 16:02:59 +0530

    update: added config map

--- backend/main.go
+++ backend/main.go
@@ -25,7 +25,6 @@ func main() {
 		log.Println(".env not found, using container environment variables")
 	}
 
-	os.Getenv("JWT_SECRETKEY")
 	config := &storage.Config{
 		Host:     os.Getenv("DB_HOST"),
 		Port:     os.Getenv("DB_PORT"),
